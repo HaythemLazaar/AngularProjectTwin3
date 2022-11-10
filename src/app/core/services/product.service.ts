a798296f9a38cbdd614ba07cabc456657ec2938c
HaythemLazaar <haythem.lazaar1@gmail.com>
2022-11-10 09:43:32 +0100

    Products List updated

--- src/app/core/services/product.service.ts
+++ src/app/core/services/product.service.ts
@@ -8,32 +8,32 @@ export class ProductService {
   private productList: Product[] = [
     {
       id: 1,
-      title: 'T-Shirt 1',
-      price: 500,
-      nbrLike: 40,
-      description: 'Cool T-shirt',
-      category: 'kids',
-      picture: 'https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/f88c38f6-5eb3-4cd8-a509-08b59410875b/tee-shirt-sportswear-club-pour-bVD3j8.png',
-      quantity: 35
+      title: 'Jordan 11 Retro',
+      price: 300,
+      nbrLike: 22,
+      description: 'No need to journey into the jungle for their next pair of kicks. Free your child\'s wild side with a lively animal print. Hear them roar! Get yourself a matching pair with the Women\'s Air Jordan 11 Retro.',
+      category: 'men',
+      picture: 'https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/e8014cc7-306a-41d8-9842-17b83de8f5e1/jordan-11-retro-little-kids-shoes-HlkH9p.png',
+      quantity: 2
     },
     {
       id: 2,
-      title: 'T-Shirt 2',
-      price: 200,
-      nbrLike: 40,
-      description: 'Cool T-shirt',
+      title: 'Nike Air Force 1',
+      price: 400,
+      nbrLike: 72,
+      description: 'The radiance lives on in the Nike Air Force 1 \’07, the b-ball icon that puts a fresh spin on what you know best: crisp leather, bold colors and the perfect amount of flash to make you shine.',
       category: 'men',
-      picture: 'https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/f88c38f6-5eb3-4cd8-a509-08b59410875b/tee-shirt-sportswear-club-pour-bVD3j8.png',
+      picture: 'https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/e125b578-4173-401a-ab13-f066979c8848/air-force-1-big-kids-shoes-M7mcL9.png',
       quantity: 2
     },
     {
       id: 3,
-      title: 'T-Shirt 3',
-      price: 50,
-      nbrLike: 40,
-      description: 'Cool T-shirt',
+      title: 'Jordan Retro 1',
+      price: 550,
+      nbrLike: 60,
+      description: 'No need to journey into the jungle for their next pair of kicks. Free your child\'s wild side with a lively animal print. Hear them roar! Get yourself a matching pair with the Women\'s Air Jordan 11 Retro.',
       category: 'men',
-      picture: 'https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/f88c38f6-5eb3-4cd8-a509-08b59410875b/tee-shirt-sportswear-club-pour-bVD3j8.png',
+      picture: 'https://static.nike.com/a/images/c_limit,w_400,f_auto/t_product_v1/7cee5620-26ec-4139-aa52-62a4bd2e1f15/image.jpg',
       quantity: 35
     },
     {
